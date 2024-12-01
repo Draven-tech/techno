13947a92f499d21b5e190927c26eb304eba4a958
KnnGnG <kenngukong@gmail.com>
2024-12-02 01:23:49 +0800

    Update equipment-status.page.ts

--- src/app/equipment-status/equipment-status.page.ts
+++ src/app/equipment-status/equipment-status.page.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Equipment {
+  name: string;
+  isAvailable: boolean;
+}
+
 @Component({
   selector: 'app-equipment-status',
   templateUrl: './equipment-status.page.html',
   styleUrls: ['./equipment-status.page.scss'],
 })
 export class EquipmentStatusPage implements OnInit {
+  equipmentList: Equipment[] = [
+    { name: 'Treadmill', isAvailable: true },
+    { name: 'Stationary Bike', isAvailable: false },
+    { name: 'Elliptical Trainer', isAvailable: true },
+    { name: 'Bench Press', isAvailable: false },
+    { name: 'Rowing Machine', isAvailable: true },
+    { name: 'Leg Press', isAvailable: false },
+  ];
 
   constructor() { }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() { }
 }
+

48a15e6d94fa235bdcf730c19bfd7eaad313b6b4
Tomikaze1 <tomikaze36@gmail.com>
2024-12-01 20:25:26 +0800

    Update diet-plans.page.ts

--- src/app/diet-plans/diet-plans.page.ts
+++ src/app/diet-plans/diet-plans.page.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'; 
 
 @Component({
   selector: 'app-diet-plans',
   templateUrl: './diet-plans.page.html',
-  styleUrls: ['./diet-plans.page.scss'],
+  styleUrls: ['./diet-plans.page.scss']
 })
 export class DietPlansPage implements OnInit {
+  public dietPlans = [
+    
+  ];
 
-  constructor() { }
+  constructor(private router: Router) {}  
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  navigateToMenu() {
+    this.router.navigate(['/membermenu']);  
   }
-
 }

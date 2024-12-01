cf0cabcd2f4530e373847ebfd151bdaa6bf7349c
Zee <zack88340@gmail.com>
2024-12-01 22:00:44 +0800

    add loc

--- src/app/gym-locations/gym-locations.page.ts
+++ src/app/gym-locations/gym-locations.page.ts
@@ -9,9 +9,16 @@ import * as L from 'leaflet';
 export class GymLocationsPage implements OnInit {
   map!: L.Map;
 
-  mockGymLocation = {
+  mockGymLocation1 = {
     lat: 10.273828844086307,
-    lng: 123.85891171349382
+    lng: 123.85891171349382,
+    name: 'Gym Location 1'
+  };
+
+  mockGymLocation2 = {
+    lat: 10.295828844086307,
+    lng: 123.87691171349382,
+    name: 'Gym Location 2'
   };
 
   constructor() {}
@@ -22,17 +29,21 @@ export class GymLocationsPage implements OnInit {
 
   initializeMap() {
     setTimeout(() => {
-      this.map = L.map('map').setView([this.mockGymLocation.lat, this.mockGymLocation.lng], 15);
+      this.map = L.map('map').setView([this.mockGymLocation1.lat, this.mockGymLocation1.lng], 15);
 
-      // Use Esri's satellite imagery layer
       L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
         attribution: '&copy; <a href="https://www.esri.com/">Esri</a>',
       }).addTo(this.map);
 
-      L.marker([this.mockGymLocation.lat, this.mockGymLocation.lng])
+      L.marker([this.mockGymLocation1.lat, this.mockGymLocation1.lng])
         .addTo(this.map)
-        .bindPopup('Gym Location: Mock')
+        .bindPopup(this.mockGymLocation1.name)
         .openPopup();
-    }, 100); // Delay to ensure map div is rendered
+
+      L.marker([this.mockGymLocation2.lat, this.mockGymLocation2.lng])
+        .addTo(this.map)
+        .bindPopup(this.mockGymLocation2.name)
+        .openPopup();
+    }, 100);
   }
 }
